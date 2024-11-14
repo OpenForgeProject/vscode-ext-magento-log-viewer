be26c0c039a1855993e404809708723dc43f388e
Mathias Elle <m.elle@dermatz.de>
2024-11-15 00:11:30 +0100

    Add configuration option for Magento root folder selection

--- src/extension.ts
+++ src/extension.ts
@@ -15,6 +15,19 @@ export function activate(context: vscode.ExtensionContext) {
 
 	if (isMagentoProject === 'Please select') {
 		vscode.window.showInformationMessage('Ist dies ein Magento Projekt?', 'Ja', 'Nein').then(selection => {
+			if (selection === 'Ja') {
+				vscode.window.showInformationMessage('Bitte wählen Sie den Magento Root Ordner aus.', 'Jetzt Magento Root auswählen').then(buttonSelection => {
+					if (buttonSelection === 'Jetzt Magento Root auswählen') {
+						vscode.window.showOpenDialog({ canSelectFolders: true, canSelectMany: false, openLabel: 'Select Magento Root Folder' }).then(folderUri => {
+							if (folderUri && folderUri[0]) {
+								config.update('magentoLogViewer.magentoRoot', folderUri[0].fsPath, vscode.ConfigurationTarget.Global).then(() => {
+									vscode.window.showInformationMessage('Magento Root Ordner erfolgreich gespeichert!');
+								});
+							}
+						});
+					}
+				});
+			}
 			config.update('magentoLogViewer.isMagentoProject', selection, vscode.ConfigurationTarget.Global);
 		});
 	}
