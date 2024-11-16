22ad16641c28a10fafb62507141a5bec90c6aad6
Mathias Elle <m.elle@dermatz.de>
2024-11-17 00:40:24 +0100

    Refactor LogFile constructor to simplify super call and improve readability

--- src/logViewer.ts
+++ src/logViewer.ts
@@ -175,7 +175,7 @@ export class LogFile extends vscode.TreeItem {
     public readonly command?: vscode.Command,
     public children?: LogFile[]
   ) {
-    super(label, collapsibleState as vscode.TreeItemCollapsibleState);
+    super(label, collapsibleState);
     this.description = this.label.match(/\(\d+\)/)?.[0] ?? '';
     this.label = this.label.replace(/\(\d+\)/, '').trim();
   }
